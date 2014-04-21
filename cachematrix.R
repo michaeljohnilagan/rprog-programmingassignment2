49cce7ef596d7a5eecc4df3d477dec58b455679e
mjsilagan <ilaganmichael@yahoo.com>
2014-04-21 17:51:46 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,34 @@
+## this R script contains two functions, 'makeCacheMatrix' and 'cacheSolve'. 
+## these two functions allow caching the inverse of a matrix. 
+## caching prevents having the same matrix inverted more than once.
+
+## 'makeCacheMatrix' is a function that creates a cacheable matrix.
+## 'x' is the matrix to be cached.
+## return object is a list of four functions:
+## (1) 'set', which overwrites the matrix with a new one;
+## (2) 'get', which returns the values of the matrix;
+## (3) 'setinv', which assigns its input to the inverse of the matrix; and
+## (4) 'getinv', which returns the inverted matrix.
+
+makeCacheMatrix <- function(x = matrix()){
+	inv <- NULL # when the matrix is created, inverse is null
+	set <- function(new){
+		x <<- new
+		inv <<- NULL
+	} # alters the matrix; also removes cache
+	get <- function(){
+		x
+	} # returns the matrix
+	setinv <- function(newinv){
+		inv <<- newinv
+	} # assigns its input to the inverse of the matrix
+	getinv <- function(){
+		inv
+	} # returns the inverted matrix
+	message("cached matrix created.")
+	invisible(list(set=set,get=get,setinv=setinv,getinv=getinv))
+}
+
 ## 'cacheSolve' is a function that inverts a matrix. 
 ## if a cached solution is found, it is returned; 
 ## otherwise, an inverse is calculated, cached, and returned.
